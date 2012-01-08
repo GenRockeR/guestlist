78b87db5d6c404fe31e5d629602fc42d81767175
Raul Morales <raul.mh@gmail.com>
2012-01-08 17:25:48 +0100

    GET manages index and MAC pages

--- guestlist/http.py
+++ guestlist/http.py
@@ -1,8 +1,5 @@
-'''
-Created on 04/01/2012
+'''GuestList database manager - HTTP server'''
 
-@author: rmorales
-'''
 import BaseHTTPServer
 import base64
 
@@ -11,8 +8,43 @@ import guestlist
 AUTHFILE = "/var/lib/radius/httpauth.conf"
 #AUTHFILE = "/tmp/httpauth.conf"
 
+html_page = '''<html><body>
+<a href="/">Inicio</a><br>
+{body}
+</body></html>
+'''
+
+def html_table(db):
+    schema = db.get_schema()
+    data = db.get_data()
+    html = ['<table border="1">', '<tr>']
+    html.append('<th>{0}</th>'.format(schema[0]))
+    html.append('<th>{0}</th>'.format(schema[1]))
+    html.append('</tr>')
+    for entry in data:
+        html.append('<tr>')
+        html.append('<td><a href="{0}">{0}</a></td>'.format(entry[0]))
+        html.append('<td>{0}</td>'.format(entry[1]))
+        html.append('</tr>')
+    return '\n'.join(html)
+
+def html_mac(db, mac):
+    data = db.get_data(mac)
+    data = data.pop()
+    html = '''MAC: {0}<br> Desc: {1}'''.format(data[0], data[1])
+    return html
+
+html_authorize = '''
+<form>
+Mac: <input type="text" name="mac" /><br />
+Description: <input type="text" name="description" /><br />
+<input type="submit" value="Submit" />
+</form>
+'''
+
 class ReqHandler(BaseHTTPServer.BaseHTTPRequestHandler):
     auth = None
+    server_version = 'guestlist/0.1'
     
     def validate_auth(self):
         if self.auth is None:
@@ -29,56 +61,30 @@ class ReqHandler(BaseHTTPServer.BaseHTTPRequestHandler):
                          'Basic realm="%s"' % self.server_version)
         self.send_header('Content-Type', 'text/plain')
         self.end_headers()
-        self.wfile.write('Username and password requeried')
+        self.wfile.write('Valid username and password requeried')
 
-    def send_index(self):
-        gl = guestlist.GuestList(guestlist.DBFILE)
-        schema = gl.get_schema()
-        data = gl.get_data()
-        html = ['<html><body>', '<table border="1">', '<tr>']
-        html.append('<th>{0}</th>'.format(schema[0]))
-        html.append('<th>{0}</th>'.format(schema[1]))
-        html.append('</tr>')
-        for entry in data:
-            html.append('<tr>')
-            html.append('<td><a href="{0}">{0}</a></td>'.format(entry[0]))
-            html.append('<td>{0}</td>'.format(entry[1]))
-            html.append('</tr>')
-        html.append('</body></html>')
-        body = '\n'.join(html)
-        
+    def send_html(self, body):
         self.send_response(200)
         self.send_header('Content-Type', 'text/html')
         self.end_headers()
-        self.wfile.write(body)
-
-    def send_authorize(self):
-        html = '''<html><body>
-<form>
-Mac: <input type="text" name="mac" /><br />
-Description: <input type="text" name="description" /><br />
-<input type="submit" value="Submit" />
-</form>
-</body></html>'''
-        self.send_response(200)
-        self.send_header('Content-Type', 'text/html')
-        self.end_headers()
-        self.wfile.write(html)
+        self.wfile.write(html_page.format(body=body))
 
     def do_GET(self):
         if not self.validate_auth():
             self.send_auth_request()
             return
         
+        db = guestlist.GuestList(guestlist.DBFILE)
+        
         if self.path == '/':
-            self.send_index()
+            self.send_html(html_table(db))
             return
-        
-        if self.path == '/authorize':
-            self.send_authorize()
+        try:
+            html = html_mac(db, self.path.lstrip('/'))
+        except ValueError:
+            self.send_error(404)
             return
-        
-        self.send_error(404)
+        self.send_html(html)
    
 if __name__ == '__main__':
     addr = ('', 8080)
